d632959eae17cd3db2a48d197f4f3c60f0f77ddb
zeberity123 <darudayu123@gmail.com>
2024-05-13 20:43:26 +0900

    arrage

---
+++ min/mingpt/utils.py
@@ -0,0 +1,109 @@
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.utils.data import Dataset
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def top_k_logits(logits, k):
+    v, ix = torch.topk(logits, k)
+    out = logits.clone()
+    out[out < v[:, [-1]]] = -float('Inf')
+    return out
+
+@torch.no_grad()
+def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
+    """
+    take a conditioning sequence of indices in x (of shape (b,t)) and predict the next token in
+    the sequence, feeding the predictions back into the model each time. Clearly the sampling
+    has quadratic complexity unlike an RNN that is only linear, and has a finite context window
+    of block_size, unlike an RNN that has an infinite context window.
+    """
+    block_size = model.get_block_size()
+    model.eval()
+    for k in range(steps):
+        x_cond = x if x.size(1) <= block_size else x[:, -block_size:] # crop context if needed
+        logits, _ = model(x_cond)
+        # pluck the logits at the final step and scale by temperature
+        logits = logits[:, -1, :] / temperature
+        # optionally crop probabilities to only the top k options
+        if top_k is not None:
+            logits = top_k_logits(logits, top_k)
+        # apply softmax to convert to probabilities
+        probs = F.softmax(logits, dim=-1)
+        # sample from the distribution or take the most likely
+        if sample:
+            ix = torch.multinomial(probs, num_samples=1)
+        else:
+            _, ix = torch.topk(probs, k=1, dim=-1)
+        # append to the sequence and continue
+        x = torch.cat((x, ix), dim=1)
+
+    return x
+
+
+class ImageDataset(Dataset):
+    """
+    wrap up the pytorch CIFAR-10 dataset into our own, which will convert images into sequences of integers
+    """
+    
+    def __init__(self, pt_dataset, clusters, perm=None):
+        self.pt_dataset = pt_dataset
+        self.clusters = clusters
+        self.perm = torch.arange(32*32) if perm is None else perm
+        
+        self.vocab_size = clusters.size(0)
+        self.block_size = 32*32 - 1
+        
+    def __len__(self):
+        return len(self.pt_dataset)
+
+    def __getitem__(self, idx):
+        x, y = self.pt_dataset[idx]
+        x = torch.from_numpy(np.array(x)).view(-1, 3) # flatten out all pixels
+        x = x[self.perm].float() # reshuffle pixels with any fixed permutation and -> float
+        a = ((x[:, None, :] - self.clusters[None, :, :])**2).sum(-1).argmin(1) # cluster assignments
+        return a[:-1], a[1:] # always just predict the next one in the sequence
+
+
+class TrainerConfig:
+    # optimization parameters
+    max_epochs = 10
+    batch_size = 64
+    learning_rate = 3e-4
+    betas = (0.9, 0.95)
+    grad_norm_clip = 1.0
+    weight_decay = 0.1 # only applied on matmul weights
+    # learning rate decay params: linear warmup followed by cosine decay to 10% of original
+    lr_decay = False
+    warmup_tokens = 375e6 # these two numbers come from the GPT-3 paper, but may not be good defaults elsewhere
+    final_tokens = 260e9 # (at what point we reach 10% of original LR)
+    # checkpoint settings
+    ckpt_path = None
+    num_workers = 0 # for DataLoader
+
+    def __init__(self, **kwargs):
+        for k,v in kwargs.items():
+            setattr(self, k, v)
+
+
+def kmeans(x, ncluster, niter=10):
+    N, D = x.size()
+    c = x[torch.randperm(N)[:ncluster]] # init clusters at random
+    for i in range(niter):
+        # assign all pixels to the closest codebook element
+        a = ((x[:, None, :] - c[None, :, :])**2).sum(-1).argmin(1)
+        # move each codebook element to be the mean of the pixels that assigned to it
+        c = torch.stack([x[a==k].mean(0) for k in range(ncluster)])
+        # re-assign any poorly positioned codebook elements
+        nanix = torch.any(torch.isnan(c), dim=1)
+        ndead = nanix.sum().item()
+        print('done step %d/%d, re-initialized %d dead clusters' % (i+1, niter, ndead))
+        c[nanix] = x[torch.randperm(N)[:ndead]] # re-init dead clusters
+    return c
---
+++ min/runSlurmStep5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=dli_assessment_step5
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1       
+#SBATCH --cpus-per-task=32 ### Number of threads per task (OMP threads)
+#SBATCH -o /dli/megatron/logs/%j.out
+#SBATCH -e /dli/megatron/logs/%j.err
+
+# Number of nodes
+NUM_NODES=2
+# Number of GPUs per node
+NUM_GPUS=2
+
+deepspeed --num_nodes=${NUM_NODES} --hostfile /dli/minGPT/minGPT/hostfile --num_gpus=${NUM_GPUS} /dli/minGPT/minGPT/runStep5.py \
+    --deepspeed \
+    --deepspeed_config /dli/minGPT/minGPT/ds_config_step5.json
